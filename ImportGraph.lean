76d8d064c56aa564a4ff49ef2bee52337bd70498
Jon Eugster <eugster.jon@gmail.com>
2023-12-21 01:40:01 +0100

    typo

--- ImportGraph.lean
+++ ImportGraph.lean
@@ -2,7 +2,7 @@ import ImportGraph.Cli
 import ImportGraph.Imports
 import ImportGraph.Lean.List
 import ImportGraph.Lean.NameMap
+import ImportGraph.Lean.Process
 import ImportGraph.Lean.TermUnsafe
 import ImportGraph.Name
-import ImportGraph.Process
 import ImportGraph.RequiredModules
