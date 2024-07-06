01779a076d9afbe28fe0a211719a562f2c18dc1e
damiano <adomani@gmail.com>
2024-07-06 18:18:01 +0200

    rename: `#minimize_imports` to `#min_imports`
    [Zulip thread](https://leanprover.zulipchat.com/#narrow/stream/144837-PR-reviews/topic/.2314437.20--.20the.20.60.23min_imps.60.20command/near/449446854)

--- ImportGraph/Imports.lean
+++ ImportGraph/Imports.lean
@@ -13,7 +13,7 @@ import ImportGraph.RequiredModules
 Provides the commands
 
 * `#redundant_imports` which lists any transitively redundant imports in the current module.
-* `#minimize_imports` which attempts to construct a minimal set of imports for the declarations
+* `#min_imports` which attempts to construct a minimal set of imports for the declarations
   in the current file.
   (Must be run at the end of the file. Tactics and macros may result in incorrect output.)
 * `#find_home decl` suggests files higher up the import hierarchy to which `decl` could be moved.
@@ -213,7 +213,7 @@ This must be run at the end of the file,
 and is not aware of syntax and tactics,
 so the results will likely need to be adjusted by hand.
 -/
-elab "#minimize_imports" : command => do
+elab "#min_imports" : command => do
   let imports := (← getEnv).minimalRequiredModules.qsort Name.lt
     |>.toList.map (fun n => "import " ++ n.toString)
   logInfo <| Format.joinSep imports "\n"
