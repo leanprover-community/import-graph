7ef514b2ed54fc740a3d2874d3bb32fafe4ac3f4
adomani <adomani@gmail.com>
2024-07-07 12:43:55 +0200

    elabCommand

--- ImportGraph/Imports.lean
+++ ImportGraph/Imports.lean
@@ -221,7 +221,7 @@ elab "#min_imports" : command => do
 -- deprecated since 2024-07-06
 elab "#minimize_imports" : command => do
   logWarning m!"'#minimize_imports' is deprecated: please use '#min_imports'"
-  elabCommand (← `(command| #min_imports))
+  Elab.Command.elabCommand (← `(command| #min_imports))
 
 /--
 Find locations as high as possible in the import hierarchy
