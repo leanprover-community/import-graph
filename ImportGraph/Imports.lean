13761a4cea8270143c3ef2e45b386b2fa3bd2909
Mauricio Collares <mauricio@collares.org>
2025-02-06 13:52:19 +0100

    fix: avoid using compile_time_search_path%

--- ImportGraph/Imports.lean
+++ ImportGraph/Imports.lean
@@ -188,7 +188,7 @@ def unusedTransitiveImports (amongst : List Name) (verbose : Bool := false) : Co
     amongst.filter (fun m => unused.contains m))
 
 def Core.withImportModules (modules : Array Name) {α} (f : CoreM α) : IO α := do
-  searchPathRef.set compile_time_search_path%
+  initSearchPath (← findSysroot)
   unsafe Lean.withImportModules (modules.map (fun m => {module := m})) {} (trustLevel := 1024)
     fun env => Prod.fst <$> Core.CoreM.toIO
         (ctx := { fileName := "<CoreM>", fileMap := default }) (s := { env := env }) do f
