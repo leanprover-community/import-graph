906eac2b8a4ac89764f87c0391ebd9491a487a9b
Kim Morrison <kim@tqft.net>
2025-04-10 16:40:44 +1000

    fix merge

--- ImportGraph/UnusedTransitiveImports.lean
+++ ImportGraph/UnusedTransitiveImports.lean
@@ -2,15 +2,6 @@ import ImportGraph.Imports
 
 open Lean
 
-<<<<<<< HEAD
-def Core.withImportModules (modules : Array Name) {α} (f : CoreM α) : IO α := do
-  initSearchPath (← findSysroot)
-  unsafe Lean.withImportModules (modules.map (fun m => {module := m})) {} (trustLevel := 1024)
-    fun env => Prod.fst <$> Core.CoreM.toIO
-        (ctx := { fileName := "<CoreM>", fileMap := default }) (s := { env := env }) do f
-
-=======
->>>>>>> origin/main
 /--
 `lake exe unused_transitive_imports m1 m2 ...`
 
