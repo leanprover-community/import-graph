a1a0b4d037edcbacb09b215cb2cea4210c0d8cfc
Jon Eugster <eugster.jon@gmail.com>
2026-02-01 22:37:22 +0100

    add deprecation warnings for imports mentioned in README

--- ImportGraph.lean
+++ ImportGraph.lean
@@ -11,10 +11,10 @@ public import ImportGraph.Imports.Unused
 public import ImportGraph.Lean.Environment
 public import ImportGraph.Lean.Name
 public import ImportGraph.Lean.WithImportModules
-public import ImportGraph.Tools
-public import ImportGraph.Tools.FindHome
-public import ImportGraph.Tools.ImportDiff
-public import ImportGraph.Tools.MinImports
-public import ImportGraph.Tools.RedundantImports
+public meta import ImportGraph.Tools
+public meta import ImportGraph.Tools.FindHome
+public meta import ImportGraph.Tools.ImportDiff
+public meta import ImportGraph.Tools.MinImports
+public meta import ImportGraph.Tools.RedundantImports
 public import ImportGraph.Util.CurrentModule
 public import ImportGraph.Util.FindSorry
