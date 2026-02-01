506ea796fb0e1f182b0ee893a106b4b234cc3950
Jon Eugster <eugster.jon@gmail.com>
2026-02-01 22:39:21 +0100

    cleanup

--- ImportGraph/FromSource.lean
+++ ImportGraph/FromSource.lean
@@ -8,4 +8,3 @@ open Lean
 -- deprecated 2026-02-01
 #eval do
   logWarning "`ImportGraph.FromSource` is deprecated! use `import ImportGraph.Imports.FromSource` instead."
-  pure ()
--- ImportGraph/Imports.lean
+++ ImportGraph/Imports.lean
@@ -8,4 +8,3 @@ open Lean
 -- deprecated 2026-02-01
 #eval do
   logWarning "`ImportGraph.Imports` is deprecated! use `import ImportGraph.Tools` instead."
-  pure ()
