cfb8e5eaa67fc5054a9a0e2efa5de42a6a381350
thorimur <68410468+thorimur@users.noreply.github.com>
2026-08-31 13:29:47 -0400

    chore: root

--- ImportGraph.lean
+++ ImportGraph.lean
@@ -21,6 +21,7 @@ public import ImportGraph.Shake.Algebra
 public import ImportGraph.Shake.Basic
 public import ImportGraph.Shake.Core
 public import ImportGraph.Shake.EnvExtension
+public import ImportGraph.Shake.Workspace
 public meta import ImportGraph.Tools
 public meta import ImportGraph.Tools.FindHome
 public meta import ImportGraph.Tools.ImportDiff
