0a391d1384f12c0e3dda71f73c5b2123cd1b270d
thorimur <68410468+thorimur@users.noreply.github.com>
2026-08-31 10:40:14 -0400

    chore: test, fix

---
+++ ImportGraphTest/Widget/GoToModule/Decls.lean
@@ -0,0 +1,13 @@
+module
+
+public section
+--     | ← cursor lands (just before) here (in one test)
+example : True := trivial
+
+def foo := true
+
+example : True := trivial
+
+def bar := false
+
+example : True := trivial
