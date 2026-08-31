a079da4b7b324ab8d5d004303bfe67e0b9b8485a
thorimur <68410468+thorimur@users.noreply.github.com>
2026-08-31 12:23:54 -0400

    chore: root

--- ImportGraph.lean
+++ ImportGraph.lean
@@ -9,7 +9,9 @@ public import ImportGraph.Imports.ImportGraph
 public import ImportGraph.Imports.Redundant
 public import ImportGraph.Imports.RequiredModules
 public import ImportGraph.Imports.Unused
+public import ImportGraph.Lake
 public import ImportGraph.Lean.Environment
+public import ImportGraph.Lean.Json
 public import ImportGraph.Lean.Name
 public import ImportGraph.Lean.WithImportModules
 public import ImportGraph.Shake.Algebra
@@ -22,3 +24,8 @@ public meta import ImportGraph.Tools.MinImports
 public meta import ImportGraph.Tools.RedundantImports
 public import ImportGraph.Util.CurrentModule
 public import ImportGraph.Util.FindSorry
+public import ImportGraph.WorkspaceModel.Base
+public import ImportGraph.WorkspaceModel.Build
+public import ImportGraph.WorkspaceModel.Emit
+public import ImportGraph.WorkspaceModel.Model
+public import ImportGraph.WorkspaceModel.Summary
