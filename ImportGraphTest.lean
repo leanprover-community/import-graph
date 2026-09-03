10520d9365480938204e8a2a9967fa73160a2570
thorimur <68410468+thorimur@users.noreply.github.com>
2026-09-03 17:24:13 -0400

    chore: clean up after bad merge

--- ImportGraphTest.lean
+++ ImportGraphTest.lean
@@ -11,6 +11,5 @@ import ImportGraphTest.ToTarget
 import ImportGraphTest.Widget.Copy
 import ImportGraphTest.WithSorry.Def
 import ImportGraphTest.WithSorry.Thm
-import ImportGraphTest.Widget.GoToModule.Msg
 import ImportGraphTest.Widget.Collapsible
 import ImportGraphTest.Widget.GoToModule.Msg
