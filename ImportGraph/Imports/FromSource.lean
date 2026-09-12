8d69e1b92287b5f77acec69af0a7a3bcd40c068e
thorimur <68410468+thorimur@users.noreply.github.com>
2026-09-12 02:37:54 -0400

    chore: add disclaimer

--- ImportGraph/Imports/FromSource.lean
+++ ImportGraph/Imports/FromSource.lean
@@ -18,7 +18,7 @@ as an alternative to `Environment`-based analysis (e.g. in `ImportGraph.Imports`
 - `parseCurrentHeader`: parse the imports of the current file from the `FileMap`
 - `modToRelFilePath`: like `modToFilePath`, but does not insert a leading file separator
 - `findTransitiveImportsFromSource`: Compute a nameset of the transitive closure of imports from
-  source files
+  source files. Note, however, that this does not respect the module system.
 -/
 
 public section
