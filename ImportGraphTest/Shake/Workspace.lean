b9aaa042d60cbcaa542a6b6ae87088463faedc5d
thorimur <68410468+thorimur@users.noreply.github.com>
2026-09-21 21:49:22 -0400

    chore: test docs

--- ImportGraphTest/Shake/Workspace.lean
+++ ImportGraphTest/Shake/Workspace.lean
@@ -3,24 +3,39 @@ module
 import ImportGraph.WorkspaceModel.Build
 import Lean.Elab.Command
 
+/-!
+The following tests assume that `ImportGraph.WorkspaceModel.Build` has
+`public import ImportGraph.WorkspaceModel.Summary`, and (only) transitively,
+`public import ImportGraph.WorkspaceModel.Base`.
+
+It also tests a small artificial hierarchy in the adjacent folder `ImportGraphTest.Shake.Workspace`
+
+- modules `A`, `B`,`C` with no imports
+- module `aB`: `import A; public import B`
+- module `bc`: `import aB; import C` (receives `B` and `C` privately)
+
+The current module is excluded because it does not come from a default target and does not live
+above `bc`, which is included via `extraMods`.
+
+Recall that a public (transitive) import looks like `⠃` and a private import looks like `⠂`.
+-/
+
 open ImportGraph Lean Shake
 
 /--
-info: Has `ImportGraphTest.Shake.Workspace`: true
-
-Has `ImportGraph.WorkspaceModel.Build`: true
-
-Has `ImportGraph.WorkspaceModel.Summary`: true
+info: Has `ImportGraphTest.Shake.Workspace`: false
 
 `ImportGraph.WorkspaceModel.Summary` > `ImportGraph.WorkspaceModel.Build`: [⠃]
 
 `ImportGraph.WorkspaceModel.Build` > `ImportGraph.WorkspaceModel.Summary`: [⠀]
 
+`ImportGraph.WorkspaceModel.Base` > `ImportGraph.WorkspaceModel.Build`: [⠃]
+
 [A|B|C|aB|bc]
-ImportGraphTest.Shake.Workspace.A: [⠇|⠀|⠀|⠀|⠀]  ⏎
-ImportGraphTest.Shake.Workspace.B: [⠀|⠇|⠀|⠀|⠀]  ⏎
-ImportGraphTest.Shake.Workspace.C: [⠀|⠀|⠇|⠀|⠀]  ⏎
-ImportGraphTest.Shake.Workspace.aB: [⠂|⠃|⠀|⠇|⠀]  ⏎
+ImportGraphTest.Shake.Workspace.A: [⠇|⠀|⠀|⠀|⠀]
+ImportGraphTest.Shake.Workspace.B: [⠀|⠇|⠀|⠀|⠀]
+ImportGraphTest.Shake.Workspace.C: [⠀|⠀|⠇|⠀|⠀]
+ImportGraphTest.Shake.Workspace.aB: [⠂|⠃|⠀|⠇|⠀]
 ImportGraphTest.Shake.Workspace.bc: [⠀|⠂|⠂|⠂|⠇]
 -/
 #guard_msgs in
@@ -32,7 +47,7 @@ run_cmd
   let abMod := testDir ++ `aB
   let bcMod := testDir ++ `bc
   let testMods := #[aMod, bMod, cMod, abMod, bcMod]
-  let extraMods := #[← getMainModule, bcMod]
+  let extraMods := #[bcMod]
   let _ ← getWorkspaceModel (extraMods := extraMods)
   -- ensure no errors on cache and non-cache paths post-cache creation
   let _ ← getWorkspaceModel (extraMods := extraMods)
@@ -47,21 +62,22 @@ run_cmd
   let mainModule ← getMainModule
   let buildMod := `ImportGraph.WorkspaceModel.Build
   let summaryMod := `ImportGraph.WorkspaceModel.Summary
+  let baseMod := `ImportGraph.WorkspaceModel.Base
   msgs := msgs.push m!"Has `{mainModule}`: \
     {w.mods.any (·.name == mainModule)}"
-  msgs := msgs.push m!"Has `{buildMod}`: \
-    {w.mods.any (·.name == buildMod)}"
-  msgs := msgs.push m!"Has `{summaryMod}`: \
-    {w.mods.any (·.name == summaryMod)}"
   let some buildModIdx := w.getModIdx? buildMod
     | throwError "Could not find index for `{buildMod}`."
   let some summaryModIdx := w.getModIdx? summaryMod
     | throwError "Could not find index for `{summaryMod}`."
+  let some baseModIdx := w.getModIdx? baseMod
+    | throwError "Could not find index for `{baseMod}`."
 
   msgs := msgs.push m!"`{summaryMod}` > `{buildMod}`: \
     {w.getMod! buildModIdx |>.transDeps.toStringAt summaryModIdx}"
   msgs := msgs.push m!"`{buildMod}` > `{summaryMod}`: \
     {w.getMod! summaryModIdx |>.transDeps.toStringAt buildModIdx}"
+  msgs := msgs.push m!"`{baseMod}` > `{buildMod}`: \
+    {w.getMod! buildModIdx |>.transDeps.toStringAt baseModIdx}"
 
   let testModIdxs ← testMods.mapM fun mod => do
     let some modIdx := w.getModIdx? mod | throwError "Could not find index for {mod}"
@@ -71,6 +87,6 @@ run_cmd
       w.getMod! modModIdx.2 |>.transDeps.brailleCellAt depModIdx.2 |>.toString
     s!"{modModIdx.1}: [{"|".intercalate chars.toList}]"
   let header := s!"[{"|".intercalate <| testMods.map (·.getString!) |>.toList}]"
-  msgs := msgs.push m!"{header}\n{"  \n".intercalate <| testDepStrings.toList}"
+  msgs := msgs.push m!"{header}\n{"\n".intercalate <| testDepStrings.toList}"
 
   logInfo <| m!"\n\n".joinSep msgs.toList
