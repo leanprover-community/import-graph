3225139e3affea5534de9c05c0136b3bd38cfdbf
thorimur <68410468+thorimur@users.noreply.github.com>
2026-09-25 16:16:01 -0400

    fix: consider modules `i < j` in `providersByLib` if `i ∈ j.prevs`, not if `i.prevs ⊆ j.prevs`
    - also compare among library prevs when ranking

--- ImportGraph/Shake/Workspace.lean
+++ ImportGraph/Shake/Workspace.lean
@@ -87,12 +87,12 @@ def DeclNeeds.toSimultaneousImportNeeds
   return declImportNeeds
 
 /-- Computes a mapping `LibIdx → Array ModIdx` assigning to each library in the workspace model an
-array of modules which provide `needs : ImportNeeds` and are minimal (by sets of previous modules).
-Modules which provide strict supersets of other candidate modules are knocked out.
+array of modules which provide `needs : ImportNeeds` and are minimal. Namely, provider modules
+which are downstream of other provider modules (ignoring module system annotations) are knocked out.
 
 It then ranks the "winning" modules first by import depth
 *among imports from the same library* (i.e. not counting imports of upstream libraries towards the
-depth), then by number of previous modules, then alphabetically.
+depth), then by number of previous modules from the same library, then by absolute number of previous modules, then alphabetically.
 
 `league : ModuleBitset` specifies the modules among which the competition is conducted. If `none`,
 this is all modules. -/
@@ -102,11 +102,13 @@ def ImportNeeds.providersByLib (w : WorkspaceModel) (needs : ImportNeeds)
   let mut minimals : Std.HashMap LibIdx (Array (Option (ModIdx × ModuleBitset))) := {}
   for h : i in 0...(Hierarchy.size w) do
     if needs.isProvidedBy w[i] && league.elim true (·.has i) then
-      let iLibPrevs := (w.getMod! i).prevs
-      minimals := minimals.incorporateBelowAt? (w.libIdxOfModIdx! i) (i, iLibPrevs)
-        fun (_, iPrevs) (_, jPrevs) => iPrevs.lt jPrevs
+      minimals := minimals.incorporateBelowAt? (w.libIdxOfModIdx! i) (i, (w.getMod! i).prevs)
+        fun (i, _) (_, jPrevs) => jPrevs.has i
   return minimals.map fun libIdx arr => (arr.reduceOption.qsort fun (i,pᵢ) (j,pⱼ) =>
     (compare (w.libDepth! i libIdx) (w.libDepth! j libIdx)) -- passing `libIdx` only for efficiency
+      |>.then (compare
+        (pᵢ ∩ (w.getLib! libIdx |>.mods)).size
+        (pⱼ ∩ (w.getLib! libIdx |>.mods)).size)
       |>.then (compare pᵢ.size pⱼ.size)
       |>.then (Name.cmp (w.getMod! i).name (w.getMod! j).name) -- for stability if all else fails
       |>.isLT).map (·.1)
